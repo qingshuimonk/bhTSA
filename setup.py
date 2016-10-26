95d8ca917ebb63fc5fa2d96f475a06a8b96c67ff
qingshuimonk <hbhzhuce@gmail.com>
2016-10-25 22:30:18 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bhtsa',
+      version='0.1',
+      description='A Twitter Sentiment Analyzer',
+      url='https://github.com/qingshuimonk/bhtsa',
+      author='qingshuimonk',
+      author_email='bh163@duke.edu',
+      license='MIT',
+      packages=['bhtsa'],
+      zip_safe=False)
