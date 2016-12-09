69894175ef83f985e47fee3cbf8cba88ea7e7dbe
Davies Odu <davies_odu@yahoo.com>
2016-12-08 19:59:53 -0500

    spelling correction

--- bhtsa/process_twt.py
+++ bhtsa/process_twt.py
@@ -1,6 +1,45 @@
 import re
 import os
+from collections import Counter
 
+#spell correction
+
+#get probability of a word
+def tokenize(text): return re.findall(r'\w+', text.lower())
+
+WORDS  = Counter(tokenize(open(os.path.join(os.path.dirname(__file__), os.pardir, 'data', 'big.txt')).read()))
+
+def Prob(word, N= sum(WORDS.values())):
+    return WORDS[word]/N
+#-------------------
+def correction(word):
+    return max(candidates(word), key=Prob)
+
+def candidates(word):
+    return (known([word]) or known(edit1(word)) or known(edit2(word)) or [word])
+
+def known(words):
+    return set(w for w in words if w in WORDS)
+
+# word distances to known words
+def edit1(word):
+    letters    = 'abcdefghijklmnopqrstuvwxyz'
+    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
+    deletes    = [L + R[1:]               for L, R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
+    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+    inserts    = [L + c + R               for L, R in splits for c in letters]
+    return set(deletes + transposes + replaces + inserts)
+
+def edit2(word): 
+    return (e2 for e1 in edit1(word) for e2 in edit1(e1))
+
+
+def spell_correct(twt):
+    for word in twt.split(' '):
+        twt = twt.replace(word.strip(), correction(word)) 
+    return twt
+#-- test autocorrect 
 
 def reduce_redundancy(twt):
     pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
